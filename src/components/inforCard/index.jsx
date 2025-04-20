f46549ddb60227c6e70b256d584bd6f881fec8a7
Yilbert Luna <yilbert.luna.e@gmail.com>
2025-04-19 20:52:42 -0400

    fix(redirect): change id

--- src/components/inforCard/index.jsx
+++ src/components/inforCard/index.jsx
@@ -4,7 +4,7 @@ export default function InfoCard({ data, fields, styles, transformField, pathnam
     return (
         <div className={`flex justify-between items-center w-full max-w-2xl bg-white shadow-md rounded-lg p-4 border gap-1 ${styles?.container || ""}`}>
             {fields.map((field, index) => {
-                const id = data.id_tramite || data.pers_id
+                const id = data.id_tramite || data.pers_cedula
                 return (
                     <Link href={`${pathname === "/registro/busqueda-solicitud" ? "/registro/tramite" : "/registro/persona"}/${id}`} key={index}>
                         <span
